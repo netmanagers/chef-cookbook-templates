88d8849e4e753df2405c08810c232ada34cf2dc7
Javier Bértoli <javier@netmanagers.com.ar>
2014-05-05 06:42:56 -0300

    Add initial package and config resources

--- templates/pack_conf-cookbook/attributes/default.rb
+++ templates/pack_conf-cookbook/attributes/default.rb
@@ -3,18 +3,19 @@
 # Attribute:: default
 #
 
-default['xxx_template_xxx']['package'] = 'xxx_template_xxx'
-default['xxx_template_xxx']['version'] = nil
-default['xxx_template_xxx']['absent'] = false
+default['xxx_template_xxx']['package_name'] = 'xxx_template_xxx'
+default['xxx_template_xxx']['package_version'] = nil
+default['xxx_template_xxx']['package_action'] = 'install'
 
-default['xxx_template_xxx']['config_dir'] = '/etc/xxx_template_xxx'
-default['xxx_template_xxx']['config_dir_source'] = nil
-default['xxx_template_xxx']['config_dir_purge'] = false
-
-default['xxx_template_xxx']['config_file'] = '/etc/xxx_template_xxx/xxx_template_xxx.conf'
-default['xxx_template_xxx']['config_file_mode'] = 0644
+default['xxx_template_xxx']['config_file_path'] = '/etc/xxx_template_xxx/xxx_template_xxx.conf'
+default['xxx_template_xxx']['config_file_mode'] = '0644'
 default['xxx_template_xxx']['config_file_owner'] = 'root'
 default['xxx_template_xxx']['config_file_group'] = 'root'
 
 default['xxx_template_xxx']['config_file_source'] = nil
 default['xxx_template_xxx']['config_file_template'] = nil
+
+default['xxx_template_xxx']['config_dir_path'] = '/etc/xxx_template_xxx'
+default['xxx_template_xxx']['config_dir_source'] = nil
+default['xxx_template_xxx']['config_dir_purge'] = false
+default['xxx_template_xxx']['config_dir_recurse'] = false
--- templates/pack_conf-cookbook/recipes/default.rb
+++ templates/pack_conf-cookbook/recipes/default.rb
@@ -2,28 +2,65 @@
 # Cookbook Name:: xxx_template_xxx-coobook
 # Recipe:: default
 #
-# Copyright (C) 2014, Jorge Espada
+# Copyright (C) 2014, Javier Bertoli
 #
 #
 
-# Example to show external dependencies (depends 'foo') on metadata.rb
-# include_recipe 'foo'
-
-
-begin
-  include_recipe "xxx_template_xxx::#{node['xxx_template_xxx']['install_method']}"
-rescue Chef::Exceptions::RecipeNotFound
-  Chef::Application.fatal! "'#{node['xxx_template_xxx']['install_method']}' is not a valid installation method for the xxx_template_xxx cookbook!"
+# Package management
+#
+package 'xxx_template_xxx' do
+  package_name node['xxx_template_xxx']['package_name']
+  version node['xxx_template_xxx']['package_version']
+  action node['xxx_template_xxx']['package_action']
 end
 
-template '/etc/xxx_template_xxx.conf' do
-  source 'xxx_template_xxx.conf.erb'
-  owner  'root'
-  group  'root'
-  mode   '0644'
-  variables(
-    :server_opts  => node['xxx_template_xxx']['server_opts'].to_hash,
-    :session_opts => node['xxx_template_xxx']['session_opts'].to_hash,
-    :window_opts  => node['xxx_template_xxx']['window_opts'].to_hash
-  )
+# Configuration directory management
+#
+remote_directory 'xxx_template_xxx.dir' do
+  path node['xxx_template_xxx']['config_dir_path']
+  if [':remove', ':purge', ':delete'].include? node['xxx_template_xxx']['package_action']
+    action :delete
+  else
+    action :create
+  end
+  only_if node['xxx_template_xxx']['config_dir_source']
+  purge node['xxx_template_xxx']['config_dir_purge']
+  recursive node['xxx_template_xxx']['config_dir_recurse']
+  files_owner node['xxx_template_xxx']['config_file_owner']
+  files_group node['xxx_template_xxx']['config_file_group']
+end
+
+# We can't specify both template && source for a file
+#
+if node['xxx_template_xxx']['config_file_template'] && node['xxx_template_xxx']['config_file_source']
+  Chef::Application.fatal! "['xxx_template_xxx']['config_file_template'] && ['xxx_template_xxx']['config_file_source'] can't both be set at the same time"
+end
+
+# If we use a template
+template 'xxx_template_xxx.conf' do
+  path node['xxx_template_xxx']['config_file_path']
+  if [':remove', ':purge', ':delete'].include? node['xxx_template_xxx']['package_action']
+    action :delete
+  else
+    action :create
+  end
+  only_if node['xxx_template_xxx']['config_file_template']
+  source "#{node['xxx_template_xxx']['config_file_template']}.erb"
+  owner node['xxx_template_xxx']['config_file_owner']
+  group node['xxx_template_xxx']['config_file_group']
+  mode node['xxx_template_xxx']['config_file_mode']
+end
+
+# If we source a file
+cookbook_file 'xxx_template_xxx.conf' do
+  path node['xxx_template_xxx']['config_file_path']
+  if [':remove', ':purge', ':delete'].include? node['xxx_template_xxx']['package_action']
+    action :delete
+  else
+    action :create
+  end
+  only_if node['xxx_template_xxx']['config_file_source']
+  owner node['xxx_template_xxx']['config_file_owner']
+  group node['xxx_template_xxx']['config_file_group']
+  mode node['xxx_template_xxx']['config_file_mode']
 end
