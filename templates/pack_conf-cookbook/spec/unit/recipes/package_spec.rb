a5659f4092e99b7992ccdd4a75d952e986c19f28
Javier Bértoli <javier@netmanagers.com.ar>
2014-05-08 19:01:07 -0300

    Make test descriptions more friendly and clear

--- templates/pack_conf-cookbook/spec/unit/recipes/package_spec.rb
+++ templates/pack_conf-cookbook/spec/unit/recipes/package_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../spec_helper'
 
 describe 'xxx_template_xxx::default' do
-  context 'when attributes are the cookbook\'s default' do
+  context 'when the cookbook is used with default attributes' do
     let(:chef_run) do
       runner = ChefSpec::Runner.new(
         :platform => 'centos',
@@ -10,17 +10,17 @@ describe 'xxx_template_xxx::default' do
       runner.converge('xxx_template_xxx::default')
     end
 
-    it 'installs the distro\'s package' do
+    it 'installs the distro-provided package' do
       expect(chef_run).to install_package('xxx_template_xxx')
     end
-    it 'leave configuration as the distro\'s package specifies' do
+    it 'installs the distro-provided configuration files and dirs' do
       expect(chef_run).to_not create_template('xxx_template_xxx.conf')
       expect(chef_run).to_not create_remote_directory('xxx_template_xxx.dir')
       expect(chef_run).to_not create_cookbook_file('xxx_template_xxx.conf')
     end
   end
 
-  context 'when [\'package_version\'] attribute is set' do
+  context 'when the [\'package_version\'] attribute is given a value' do
     let(:chef_run) do
       runner = ChefSpec::Runner.new(
         :platform => 'centos',
@@ -30,12 +30,17 @@ describe 'xxx_template_xxx::default' do
       runner.converge('xxx_template_xxx::default')
     end
 
-    it 'installs the distro\'s package with a specific version' do
+    it 'installs the distro-provided package with the specified version' do
       expect(chef_run).to install_package('xxx_template_xxx').with(:version => '3.2.1')
+    end
+    it 'installs the distro-provided configuration files and dirs' do
+      expect(chef_run).to_not create_template('xxx_template_xxx.conf')
+      expect(chef_run).to_not create_remote_directory('xxx_template_xxx.dir')
+      expect(chef_run).to_not create_cookbook_file('xxx_template_xxx.conf')
     end
   end
 
-  context 'when [\'package_action\'] => remove' do
+  context 'when [\'package_action\'] is set to one of {remove,purge,delete}' do
     let(:chef_run) do
       runner = ChefSpec::Runner.new(
         :platform => 'centos',
@@ -48,25 +53,52 @@ describe 'xxx_template_xxx::default' do
     it 'removes the package' do
       expect(chef_run).to remove_package('xxx_template_xxx')
     end
-    it 'removes/leave the configuration directory and files (according to package instructions)' do
-      expect(chef_run).to_not delete_template('xxx_template_xxx.conf')
-      expect(chef_run).to_not delete_remote_directory('xxx_template_xxx.dir')
-      expect(chef_run).to_not delete_cookbook_file('xxx_template_xxx.conf')
+    it 'removes the configuration directory and files' do
+      expect(chef_run).to delete_template('xxx_template_xxx.conf')
+      expect(chef_run).to delete_remote_directory('xxx_template_xxx.dir')
+      expect(chef_run).to delete_cookbook_file('xxx_template_xxx.conf')
     end
   end
 
-  context 'when [\'config_file_template\'] attribute is set' do
+  context 'when providing a config template via the [\'config_file_template\'] attribute' do
     let(:chef_run) do
       runner = ChefSpec::Runner.new(
         :platform => 'centos',
         :version => '6.5'
       )
-      runner.node.set['xxx_template_xxx']['package_action'] = 'remove'
       runner.node.set['xxx_template_xxx']['config_file_template'] = 'some_template'
       runner.converge('xxx_template_xxx::default')
     end
-    it 'removes the configuration file' do
-      expect(chef_run).to delete_template('xxx_template_xxx.conf')
+
+    it 'installs the distro-provided configuration __DIRS__' do
+      expect(chef_run).to_not create_remote_directory('xxx_template_xxx.dir')
+    end
+    it 'manages the main configuration __FILE__ via the template' do
+      expect(chef_run).to create_template('xxx_template_xxx.conf')
+    end
+    it 'should not pull the main configuration __FILE__ from a source' do
+      expect(chef_run).to_not create_cookbook_file('xxx_template_xxx.conf')
+    end
+  end
+
+  context 'when pulling a config file from a source via the [\'config_file_source\'] attribute' do
+    let(:chef_run) do
+      runner = ChefSpec::Runner.new(
+        :platform => 'centos',
+        :version => '6.5'
+      )
+      runner.node.set['xxx_template_xxx']['config_file_source'] = 'some_source'
+      runner.converge('xxx_template_xxx::default')
+    end
+
+    it 'installs the distro-provided configuration __DIRS__' do
+      expect(chef_run).to_not create_remote_directory('xxx_template_xxx.dir')
+    end
+    it 'pulls the main configuration __FILE__ from a source' do
+      expect(chef_run).to create_cookbook_file('xxx_template_xxx.conf')
+    end
+    it 'should not manage the main configuration __FILE__ via the template' do
+      expect(chef_run).to_not create_template('xxx_template_xxx.conf')
     end
   end
 end
