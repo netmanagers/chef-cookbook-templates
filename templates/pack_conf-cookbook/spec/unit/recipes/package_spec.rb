3d71db6c648cc8cf6f1b76b077a02bb27173cb67
Javier Bértoli <javier@netmanagers.com.ar>
2014-05-09 14:15:13 -0300

    Add test for error condition

--- templates/pack_conf-cookbook/spec/unit/recipes/package_spec.rb
+++ templates/pack_conf-cookbook/spec/unit/recipes/package_spec.rb
@@ -13,7 +13,7 @@ describe 'xxx_template_xxx::default' do
     it 'installs the distro-provided package' do
       expect(chef_run).to install_package('xxx_template_xxx')
     end
-    it 'installs the distro-provided configuration files and dirs' do
+    it 'installs/creates the distro-provided configuration files and dirs' do
       expect(chef_run).to_not create_template('xxx_template_xxx.conf')
       expect(chef_run).to_not create_remote_directory('xxx_template_xxx.dir')
       expect(chef_run).to_not create_cookbook_file('xxx_template_xxx.conf')
@@ -33,7 +33,7 @@ describe 'xxx_template_xxx::default' do
     it 'installs the distro-provided package with the specified version' do
       expect(chef_run).to install_package('xxx_template_xxx').with(:version => '3.2.1')
     end
-    it 'installs the distro-provided configuration files and dirs' do
+    it 'installs/creates the distro-provided configuration files and dirs' do
       expect(chef_run).to_not create_template('xxx_template_xxx.conf')
       expect(chef_run).to_not create_remote_directory('xxx_template_xxx.dir')
       expect(chef_run).to_not create_cookbook_file('xxx_template_xxx.conf')
@@ -53,10 +53,28 @@ describe 'xxx_template_xxx::default' do
     it 'removes the package' do
       expect(chef_run).to remove_package('xxx_template_xxx')
     end
-    it 'removes the configuration directory and files' do
+    it 'removes the configuration files and dirs' do
       expect(chef_run).to delete_template('xxx_template_xxx.conf')
       expect(chef_run).to delete_remote_directory('xxx_template_xxx.dir')
       expect(chef_run).to delete_cookbook_file('xxx_template_xxx.conf')
+    end
+  end
+
+  context 'when setting [\'config_file_template\'] and [\'config_file_source\'] at the same time' do
+    let(:chef_run) do
+      runner = ChefSpec::Runner.new(
+        :platform => 'centos',
+        :version => '6.5'
+      )
+      runner.node.set['xxx_template_xxx']['config_file_template'] = 'some_template'
+      runner.node.set['xxx_template_xxx']['config_file_source'] = 'some_source'
+      runner.converge('xxx_template_xxx::default')
+    end
+
+    it 'raises a fatal error' do
+      expect { chef_run }.to raise_error('[\'xxx_template_xxx\'][\'config_file_template\'] && '\
+                                         '[\'xxx_template_xxx\'][\'config_file_source\'] can\'t '\
+                                         'both be set at the same time')
     end
   end
 
@@ -76,7 +94,7 @@ describe 'xxx_template_xxx::default' do
     it 'manages the main configuration __FILE__ via the template' do
       expect(chef_run).to create_template('xxx_template_xxx.conf')
     end
-    it 'should not pull the main configuration __FILE__ from a source' do
+    it 'does not pull the main configuration __FILE__ from source' do
       expect(chef_run).to_not create_cookbook_file('xxx_template_xxx.conf')
     end
   end
@@ -91,13 +109,13 @@ describe 'xxx_template_xxx::default' do
       runner.converge('xxx_template_xxx::default')
     end
 
-    it 'installs the distro-provided configuration __DIRS__' do
+    it 'installs the distro-provided configuration __DIR__' do
       expect(chef_run).to_not create_remote_directory('xxx_template_xxx.dir')
     end
     it 'pulls the main configuration __FILE__ from a source' do
       expect(chef_run).to create_cookbook_file('xxx_template_xxx.conf')
     end
-    it 'should not manage the main configuration __FILE__ via the template' do
+    it 'does not manage the main configuration __FILE__ via the template' do
       expect(chef_run).to_not create_template('xxx_template_xxx.conf')
     end
   end
